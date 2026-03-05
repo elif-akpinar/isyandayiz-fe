09f137a812d00f98afd9f632537b8ec81c30be43
Elif Akpınar <elif.akpinar269@gmail.com>
2026-03-06 00:55:49 +0300

    bug: link missing app.jsx fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Haberler from './pages/Haberler';
